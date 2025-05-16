bea74d2bff2a40bdb83328b6a1940db0d4057dc3
Deyvis2k <deyvis.santos33@gmail.com>
2025-05-16 09:12:18 -0300

    Changes

--- Build/CMakeFiles/copy_style.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/copy_style"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/copy_style.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
